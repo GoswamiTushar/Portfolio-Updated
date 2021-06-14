36aa56cfe4e9f838c13ee155c2a39f60aa28629f
GoswamiTushar <tushargoswami1510@gmail.com>
2021-06-14 20:40:21 +0530

    Projects section Changes

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,47 @@
+const techBG = {
+  Python: "url(../images/projects/low-qual/bg1.png)",
+  WebD: "url(../images/projects/low-qual/bg2.png)",
+  Java: "url(../images/projects/low-qual/bg3.png)",
+  IoT: "url(../images/projects/low-qual/bg4.png)",
+  ReactJS: "url(../images/projects/low-qual/bg5.png)",
+};
+const techDesc = {
+  Python:
+    "Have done numerous projects on Web-Automation, Web-Scraping and Data Mining along with handling API's",
+  WebD: "Have made projects such as Snake Game, Portfolio Website from scratch along with writing component libraties",
+  Java: "Currently doing Competitive programming with Java along with Learning JavFX",
+  IoT: "Familiar with Arduino platform, RaspberryPi, Firebase, sensor interfacing & Have designed Smart Water-Management Sys",
+  ReactJS:
+    "Currently dedicating time to learn ReactJS and build apps along with tutorials",
+};
+
+const BGs = Object.keys(techBG);
+
+var count = 0;
+changeBackground();
+
+function changeBackground() {
+  if (count === BGs.length) {
+    count = 0;
+  } else if (count < BGs.length) {
+    document.getElementById("tech-stack").style.backgroundImage =
+      techBG[BGs[count]];
+
+    document.getElementById("lan-title").innerHTML = `
+    <h1 class="font-formatted-2" style="color: rgb(54, 54, 54)">
+    ${BGs[count]}
+    <div class="divider-grey"></div>
+    </h1>`;
+
+    document.getElementById("lan-desc").innerHTML = `
+    <p class="font-formatted-1" style="color: rgb(54, 54, 54); font-weight: 000">
+    ${techDesc[BGs[count]]}</p>`;
+
+    count += 1;
+  }
+  setTimeout(changeBackground, 3000);
+}
+
 var menuButtonClicked = false;
 
 document.getElementById("menu-button").addEventListener("click", () => {
@@ -8,31 +52,3 @@ document.getElementById("menu-button").addEventListener("click", () => {
   }
   menuButtonClicked = !menuButtonClicked;
 });
-
-const techBG = {
-  Python: "url(../images/projects/bg1.png)",
-  WebD: "url(../images/projects/bg2.png)",
-  Java: "url(../images/projects/bg3.png)",
-  IoT: "url(../images/projects/bg4.png)",
-  ReactJS: "url(../images/projects/bg5.png)",
-};
-
-const BGs = Object.keys(techBG);
-var count = 0;
-//
-function changeBackground() {
-  if (count < BGs.length) {
-    document.getElementById("tech-stack").style.backgroundImage =
-      techBG[BGs[count]];
-    document.getElementById("lan-title").innerHTML = `
-    <h1 class="font-formatted-1">${BGs[count]}
-    <div class="divider-grey"></div></h1>
-    `;
-    count += 1;
-  } else if (count === BGs.length) {
-    count = 0;
-  }
-  setTimeout(changeBackground, 3000);
-}
-
-changeBackground();
